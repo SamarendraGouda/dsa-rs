9ef714b5f74ed246a816780a33659260c851b53b
Samarendra Gouda <73637398+SamarendraGouda@users.noreply.github.com>
2024-01-11 23:34:21 +0530

    feat: problem-added

---
+++ easy/219-contains-duplicate-2.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut window = HashSet::new();
+        let mut l = 0;
+
+        for (r, &num) in nums.iter().enumerate() {
+            if r as i32 - l as i32 > k {
+                window.remove(&nums[l]);
+                l += 1;
+            }
+            if window.contains(&num) {
+                return true;
+            }
+            window.insert(num);
+        }
+        false
+    }
